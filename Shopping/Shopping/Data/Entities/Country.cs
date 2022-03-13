f7944c5f7fa8ba9a0f53c700fd74be4dc15f64fc
fpcd <fpcd28@gmail.com>
2022-03-12 19:54:34 -0500

    Create CountriesController

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Pais")]
+        [MaxLength(50,ErrorMessage = "EL campo debe tener maximo {1} caracteres")]
+        [Required(ErrorMessage ="El Campo {0} es obligatorio")]
+        public string Name { get; set; }
+    }
+}
